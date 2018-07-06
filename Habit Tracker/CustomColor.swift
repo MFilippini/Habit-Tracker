0ebd90a25e9ada5e6c74cef1f0fdb5a9505254b7
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-07-06 10:20:39 -0500

    Made Custom Colors

---
+++ Habit Tracker/CustomColor.swift
@@ -0,0 +1,20 @@
+//
+//  CustomColor.swift
+//  Habit Tracker
+//
+//  Created by Michael Filippini on 7/6/18.
+//  Copyright © 2018 Michael Filippini. All rights reserved.
+//
+
+import UIKit
+
+class CustomColor: UIColor {
+
+    let red = UIColor(red: 250/255.0, green: 28/255.0, blue: 74/255.0, alpha: 1)
+    let blue = UIColor(red: 18/255.0, green: 159/255.0, blue: 205/255.0, alpha: 1)
+    let purple = UIColor(red: 201/255.0, green: 18/255.0, blue: 221/255.0, alpha: 1)
+    let green = UIColor(red: 120/255.0, green: 208/255.0, blue: 71/255.0, alpha: 1)
+    let orange = UIColor(red: 238/255.0, green: 148/255.0, blue: 0, alpha: 1)
+    let yellow = UIColor(red: 241/255.0, green: 226/255.0, blue: 10/255.0, alpha: 1)
+    
+}
---
+++ Habit Tracker/HabitCell.swift
@@ -0,0 +1,13 @@
+//
+//  HabitCell.swift
+//  Habit Tracker
+//
+//  Created by Michael Filippini on 7/5/18.
+//  Copyright © 2018 Michael Filippini. All rights reserved.
+//
+
+import UIKit
+
+class HabitCell: UICollectionViewCell {
+    
+}
