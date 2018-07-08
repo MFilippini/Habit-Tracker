8e1234a4c4d245f989a54264045d4f411e12a87c
Michael Filippini <michaelfilippini@michaels-macbook-pro-2.local>
2018-07-07 19:55:03 -0500

    color select is all working

--- Habit Tracker/AddNewHabitScreen.swift
+++ Habit Tracker/AddNewHabitScreen.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 class AddNewHabitScreen: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-
+    
     @IBOutlet weak var colorSelectorCollView: UICollectionView!
     
     let colorsArray = [Common.Global.purple,Common.Global.blue,Common.Global.green,Common.Global.yellow,Common.Global.orange,Common.Global.red]
+    
+    var selectedColor = Common.Global.blue
+    var habitName = ""
+    var habitPerDay = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
         colorSelectorCollView.dataSource = self
         colorSelectorCollView.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,14 +39,28 @@ class AddNewHabitScreen: UIViewController, UICollectionViewDelegate, UICollectio
         
         return cell
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderWidth = 5
+        cell?.layer.borderColor = UIColor.white.cgColor
+        selectedColor = colorsArray[indexPath.item]
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.layer.borderWidth = 0.5
+        cell?.layer.borderColor = Common.Global.lightGrey.cgColor
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
